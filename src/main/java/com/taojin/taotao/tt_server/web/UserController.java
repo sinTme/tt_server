09866dbd627fb87ab9b2a426abc902ffa92590e1
powek <sintme@163.com>
2017-10-24 16:10:04 +0800

    [add] redis

--- src/main/java/com/taojin/taotao/tt_server/web/UserController.java
+++ src/main/java/com/taojin/taotao/tt_server/web/UserController.java
@@ -1,6 +1,8 @@
 package com.taojin.taotao.tt_server.web;
 
 import com.taojin.taotao.tt_server.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserController {
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public User login()
     {
+        redisTemplate.opsForValue().set("name","qqq");
         User user = new User();
-        user.setUserName("admin");
+        user.setUserName((String) redisTemplate.opsForValue().get("name"));
         user.setUserPassword("123");
         user.setUserPhoneNumber("11111");
         return user;
